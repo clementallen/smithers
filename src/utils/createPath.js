5895a5bc9e997608a832b52ffa9caf7929413a81
Clement Allen <clementallen313@gmail.com>
2017-05-10 17:05:44 +0100

    Removed unnecessary check in createPath (#13)

--- src/utils/createPath.js
+++ src/utils/createPath.js
@@ -2,9 +2,7 @@ export default (template, values = {}) => {
     let path = template;
 
     Object.keys(values).forEach((key) => {
-        if (Object.prototype.hasOwnProperty.call(values, key)) {
-            path = path.replace(`{${key}}`, values[key]);
-        }
+        path = path.replace(`{${key}}`, values[key]);
     });
 
     return path;
