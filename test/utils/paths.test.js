e04229c0a5ba8ee7d4e7e5c7e04482f0453859be
Clement Allen <clementallen313@gmail.com>
2017-07-05 22:02:12 +0100

    Updated deps (#22)

--- test/utils/paths.test.js
+++ test/utils/paths.test.js
@@ -2,6 +2,6 @@ import paths from '../../src/utils/paths';
 
 describe('paths', () => {
     it('should return an object', () => {
-        expect(paths).to.be.an.object;
+        expect(paths).to.be.an.instanceOf(Object);
     });
 });
